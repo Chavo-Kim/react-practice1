a089ddd88bd7aa4b1ad5fecdf40e7ffea256430b
Chavo Kim <treeboy0384@gmail.com>
2020-12-31 09:59:51 +0900

    beginner practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,90 @@
-import React from 'react';
+import React, {useMemo, useRef, useState} from 'react';
 import Hello from './Hello';
+import './App.css';
+import Wrapper from "./Wrapper";
+import Counter from "./Counter";
+import InputSample from "./inputSample";
+import UserList from "./UserList";
+import CreateUser from "./CreateUser";
+
+function countActiveUsers(users){
+    console.log('활성 사용자 수를 세는 중');
+    return users.filter(user => user.active).length;
+}
 
 function App(){
-  return(
-      <div>
-        <Hello />
-      </div>
-  );
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: ''
+    });
+
+    const {username, email} = inputs;
+    const onChange = e => {
+        const {name, value} = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    };
+    const onRemove = id => {
+        setUsers(users.filter(user => user.id !== id));
+    };
+    const [users, setUsers] = useState([{
+            id: 1,
+            username: 'velopert',
+            email: 'public.velopert@gmail.com',
+            active: true,
+        },
+        {
+            id: 2,
+            username: 'tester',
+            email: 'tester@example.com',
+            active: false,
+        },
+        {
+            id: 3,
+            username: 'liz',
+            email: 'liz@example.com',
+            active: false,
+        }]);
+
+
+    const nextId = useRef(4);
+    const onCreate = () => {
+        const user = {
+            id: nextId.current,
+            username,
+            email
+        };
+        setUsers([...users, user])
+        setInputs({
+            username: '',
+            email: ''
+        });
+        nextId.current += 1;
+    }
+    const onToggle = id => {
+        setUsers(
+            users.map(user =>
+                user.id === id ? {...user, active: !user.active} : user
+            )
+        );
+    }
+
+    const count = useMemo(() => countActiveUsers(users), [users]);
+
+    return(
+        <Wrapper>
+            <CreateUser
+                username={username}
+                email={email}
+                onChange={onChange}
+                onCreate={onCreate}
+            />
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+            <div> active users: {count} </div>
+        </Wrapper>
+    );
 }
 
 export default App;
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function Hello(){
-    return <div>안뇽하세요</div>
+function Hello({color, name, isSpecial}){
+    return <div style={{color}}>
+        {isSpecial ? <b>*</b>:null}
+        안녕하세요 {name}
+    </div>
+}
+
+Hello.defaultProps = {
+    name: '이름없음',
+    isSpecial: false
 }
 
 export default Hello;
